5931a3d1c71ddd27d081e574086b014f731055a7
Gohelboy <dwarkeshgohel1@gmail.com>
2022-08-21 16:30:57 +0530

    upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Header from "./components/Header/Header.js";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <h1>Hello</h1>
+    </div>
+  );
+}
+
+export default App;
