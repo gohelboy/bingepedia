35d872396bca2303719481d09208ac6511e0a3fe
Gohelboy <dwarkeshgohel1@gmail.com>
2023-01-10 16:26:28 +0530

    .

--- src/App.js
+++ src/App.js
@@ -4,8 +4,6 @@ import { Container } from "@mui/material";
 import Header from "./components/Header/Header";
 import Navbar from "./components/navbar/Navbar";
 
-import { GlobalProvider } from "./context/Globalstate";
-
 import Trending from "./Pages/Trending/Trending";
 import Movie from "./Pages/Movie/Movie";
 import Series from "./Pages/Series/Series";
@@ -16,23 +14,21 @@ import "./App.css";
 
 function App() {
   return (
-    <GlobalProvider>
-      <BrowserRouter>
-        <Header />
-        <div className="container">
-          <Container>
-            <Routes>
-              <Route path="/" element={<Trending />} exact />
-              <Route path="/movie" element={<Movie />} />
-              <Route path="/series" element={<Series />} />
-              <Route path="/search" element={<Search />} />
-              <Route path="/me" element={<Me />} />
-            </Routes>
-          </Container>
-        </div>
-        <Navbar />
-      </BrowserRouter>
-    </GlobalProvider>
+    <BrowserRouter>
+      <Header />
+      <div className="container">
+        <Container>
+          <Routes>
+            <Route path="/" element={<Trending />} exact />
+            <Route path="/movie" element={<Movie />} />
+            <Route path="/series" element={<Series />} />
+            <Route path="/search" element={<Search />} />
+            <Route path="/me" element={<Me />} />
+          </Routes>
+        </Container>
+      </div>
+      <Navbar />
+    </BrowserRouter>
   );
 }
 
